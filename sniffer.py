b0052d2f689c1668b2ae3b35402ccea2e8aaef90
Victor <victor3790@gmail.com>
2026-03-24 21:13:21 -0600

    Sniffer function added

--- sniffer.py
+++ sniffer.py
@@ -1,1 +1,8 @@
 from scapy.all import sniff, Raw
+
+def catch_packet(packet):
+    if packet.haslayer(Raw):
+        print(packet[Raw].load.decode('utf-8', errors='ignore'))
+
+print("Starting packet sniffer...")
+sniff(prn=catch_packet, filter="tcp port 8080", store=0)

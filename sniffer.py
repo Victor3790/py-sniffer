d2474733c3d765c47e28aa087735a9ce3d0ca53e
Victor <victor3790@gmail.com>
2026-03-27 18:19:18 -0600

    String search added

--- sniffer.py
+++ sniffer.py
@@ -2,7 +2,11 @@ from scapy.all import sniff, Raw
 
 def catch_packet(packet):
     if packet.haslayer(Raw):
-        print(packet[Raw].load.decode('utf-8', errors='ignore'))
+        print(packet.summary())
+        
+        payload = packet[Raw].load.decode('utf-8', errors='ignore')
+        if 'username' in payload:
+            print(payload[payload.find('username='):])
 
 print("Starting packet sniffer...")
 sniff(prn=catch_packet, filter="tcp port 8080", store=0)
